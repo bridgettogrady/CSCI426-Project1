58d4669f898de29067756547915c9dc0d5054e7a
sribee8 <145801438+sribee8@users.noreply.github.com>
2025-01-26 16:03:28 -0800

    AudioVisualEffects
    Added audio effects for platform interaction as well as sprite for the circle

--- CSCI426 P1/Assets/Scripts/Behavior.cs
+++ CSCI426 P1/Assets/Scripts/Behavior.cs
@@ -8,33 +8,49 @@ public class Behavior : MonoBehaviour
     private float startY;
     public float upwardForce = 10f;
     public float horzForce = 5f;
+    public AudioSource bounce;
+    public AudioSource woosh;
+    public AudioSource lose;
+    private Rigidbody2D rb;
+    public Sprite sad;
+    private Sprite normal;
+    private SpriteRenderer spriteRenderer;
     // Start is called before the first frame update
     void Start()
     {
         startX = transform.position.x;
         startY = transform.position.y;
+        rb = GetComponent<Rigidbody2D>();
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        normal = spriteRenderer.sprite;
     }
 
     // Update is called once per frame
     void Update()
     {
-
+        if(transform.position.x < -10 || transform.position.x > 10)
+        {
+            transform.position = new Vector3(startX, startY, 0);
+            rb.velocity = Vector3.zero;
+        }
     }
 
     void OnCollisionEnter2D(Collision2D col)
     {
-        Rigidbody2D rb = GetComponent<Rigidbody2D>();
         if (col.gameObject.tag == "Bounce")
         {
             rb.AddForce(Vector2.up * upwardForce, ForceMode2D.Impulse);
+            bounce.Play();
         }
         if (col.gameObject.tag == "Left")
         {
             rb.AddForce(Vector2.left * horzForce, ForceMode2D.Impulse);
+            woosh.Play();
         }
         if (col.gameObject.tag == "Right")
         {
             rb.AddForce(Vector2.right * horzForce, ForceMode2D.Impulse);
+            woosh.Play();
         }
         else if (col.gameObject.tag == "Reset")
         {
@@ -50,10 +66,19 @@ public class Behavior : MonoBehaviour
             }
             if (!otherOverlap)
             {
-                transform.position = new Vector3(startX, startY, 0);
+                spriteRenderer.sprite = sad;
+                lose.Play();
+                Invoke("Reset", 1.5f);
             }
         }
 
+    }
+
+    private void Reset()
+    {
+        spriteRenderer.sprite = normal;
+        transform.position = new Vector3(startX, startY, 1f);
+        rb.velocity = Vector3.zero;
     }
 
 
